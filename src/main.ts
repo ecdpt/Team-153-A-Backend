dd6a626471330b2e01a2cb7177470f01b34c4535
Charles <cuwagbale@yahoo.com>
2020-09-14 05:53:55 +0100

    Added database credential

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,13 @@ async function bootstrap() {
   await app.listen(port);
 }
 bootstrap();
+/*
+POSTGRES_HOST=127.0.0.1
+POSTGRES_PORT=5432
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=mysecretpassword
+POSTGRES_DATABASE=my_database
+PORT=3000
+MODE=DEV
+RUN_MIGRATIONS=true
+*/
